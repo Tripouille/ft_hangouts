b8c2a75352b7f5c3ea1a34db959f47b6f0f6b021
Tripouille <jmichel.gambard@gmail.com>
2021-07-11 19:02:55 +0200

    working on add_contact

--- app/src/main/java/com/example/ft_hangouts/MainActivity.java
+++ app/src/main/java/com/example/ft_hangouts/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.ft_hangouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+	private Button		mAddContactButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mAddContactButton = findViewById(R.id.add_contact_button);
+		mAddContactButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, AddContact.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
